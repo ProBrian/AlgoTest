cc1d28accce0eeb5a8b233feefc86c7c651514ad
jinchen <jinchen@sonicwall.com>
2016-08-16 14:59:51 +0800

    change tips of countSort.c

--- countSort.c
+++ countSort.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <string.h>
+
+void countSort(int* array, int len, int max)
+{
+    int i,j;
+    int counter[max+1];
+    int sArray[len];//this array need not to initialize
+    memset(counter, 0x0, (max+1)*sizeof(int));
+    /*count every number's count*/
+    for (i=0; i< len; i++)
+    {
+        counter[array[i]]++;
+    }
+    
+    for (i=1;i<=max;i++)
+    {
+        counter[i] += counter[i-1];
+    }
+
+    for (i=0; i<len; i++)
+    {
+        j = array[i];
+        sArray[counter[j] - 1] = array[i];
+        array[i]--;
+    }
+    memcpy(array, sArray, len*sizeof(int));
+}
 
 int main(void)
 {
     int a;
     int array[100];
-    int len;
+    int len=0;
     int i=0;
-    printf("input the max value of this array:\n");
+    printf("input the max positive value of this array:\n");
     scanf("%d", &a);
     fflush(stdin);
-    printf("input the number array need to be sort: \n");
+    printf("input the number array(positive) need to be sort: \n");
+
     do{
-        sscanf("%d", &array[i++]);
+        scanf("%d", &array[i++]);
         len++;
     }while(getchar() != '\n');
 
@@ -22,9 +50,9 @@ int main(void)
         printf("%d ", array[i]);
     }
     printf("\n");
-    /*Merge sort part*/
-    /*
-    mergeSort(array, 0, len-1);
+    
+    countSort(array, len, a);
+
     printf("Sorted array :");
     for(i=0; i<len; i++)
     {
@@ -32,5 +60,4 @@ int main(void)
     }
     printf("\n");
     return 1;
-    */
 }
